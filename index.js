0a504332a68bf6463235d35d2ae6fa901febc69c
JeftePlayGame <2102195@utrivieramaya.edu.mx>
2023-07-08 13:32:17 -0500

    4

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import App from './config/app.config.js';
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-const app = new App().app;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// Importa y registra las rutas
-import { mainRoutes } from './routes/main.routes.js';
-
-const routes = new mainRoutes();
-routes.routes(app);
+App.app.listen(port, () => console.log(`Server running on port ${port}`));
